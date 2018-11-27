10a6e2d77783ecd44933e176e103c53d26532f3d
Josh Grazda <joshuagrazda@yahoo.com>
2018-11-27 17:24:27 -0500

    SteamVR support and created new WarehouseVR scene Added: - WarehouseVR scene with SteamVR player rig Changed: - Player Input for VR is on touchpad press - Player is moved into building - Decreased intensity of lights in warehouse

---
+++ Assets/Scripts/Player/FlashlightScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class FlashlightScript : MonoBehaviour
+{
+    public Hand hand;
+
+    [Header("Light")]
+    public bool isLit;
+    public float lightIntensity;
+    public AudioClip lightClip;
+    private Light playerLight;
+
+    void Start()
+    {
+        //Player light
+        playerLight = GameObject.Find("FlashLightBeam").GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        UpdatePlayerLight();
+    }
+
+    public void ToggleLight()
+    {
+        //Set light on and off
+        isLit = !isLit;
+        //Play sound
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = lightClip;
+        audio.Play();
+    }
+
+    private void UpdatePlayerLight()
+    {
+        if(SteamVR_Input._default.inActions.Teleport.GetStateDown(hand.handType))
+        {
+            ToggleLight();
+        }
+        if (isLit)
+        {
+            playerLight.intensity = lightIntensity;
+        }
+        else
+        {
+            playerLight.intensity = 0;
+        }
+    }
+}
